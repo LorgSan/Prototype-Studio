eb6a8264e11a28a50399b16ad99f076f72b32ae9
Lorg <hikariame100@gmail.com>
2022-09-15 20:53:22 -0400

    more bugfixes but now for real

--- PS_Corn/Assets/Scripts/PanScript.cs
+++ PS_Corn/Assets/Scripts/PanScript.cs
@@ -31,33 +31,13 @@ public class PanScript : MonoBehaviour
     [SerializeField] List<CornScript> cobs = new List<CornScript>();
     [SerializeField] AudioClip readySound;
     [SerializeField] Text scoreText;
-
-    CircleCollider2D panCollider;
     float score;
     GameManager myManager;
-
-
     bool cobWasCooked = false;
 
     void Start()
     {
         myManager = GameManager.FindInstance();
-        panCollider = GetComponent<CircleCollider2D>();
-    }
-
-    void CheckCollider()
-    {
-        if (cobs.Count >= 4)
-        {
-            panCollider.enabled = !panCollider.enabled;
-        }
-        for(int x = 0; x < cobs.Count; x++)
-        {
-            if (cobs[x] == null)
-            {
-                panCollider.enabled = !panCollider.enabled;
-            }
-        }
     }
 
     public void DestroyCob()
@@ -76,51 +56,44 @@ public class PanScript : MonoBehaviour
                     cobWasCooked=true;
                     GetComponent<AudioSource>().PlayOneShot(readySound);
                     score++;
-                    scoreText.text =score.ToString();
+                    scoreText.text = score.ToString();
                 }
             }
         }
     }
-
     public void CobPlace(GameObject cob)
     {
-        if (cobs.Count < 4 || anyNull() == true)
+        if (cobs.Count < 4 || anyNull() == true) //if the amount of cobs is not more than 4 and none of them are null
         {
-            CornScript cornScript = cob.GetComponent<CornScript>();
+            CornScript cornScript = cob.GetComponent<CornScript>(); //we save its' cornscript
 
-            if (cobWasCooked==true)
+            if (cobWasCooked==true) //if cobs were at some point cooked
             {
                 for(int x = 0; x < cobs.Count; x++)
                 {
-                    if (cobs[x] == null)
+                    if (cobs[x] == null) //we're finding the created null element in the array of elements
                     {
-                        cobs[x] = cornScript;
+                        cobs[x] = cornScript; //if it is null we save the generated cob as this element in the array
+                        CobSetup(cob, cornScript); //and perform it's setup
                         break;
-                    } else if (cobs.Count < 4)
+                    } else if (cobs.Count < 4) //otherwise, if none of them a
                     {
                         cobs.Add(cornScript);
-                    } 
+                        CobSetup(cob, cornScript);
+                    }
                 }
+
             } else if (cobs.Count < 4)
             {
                 cobs.Add(cornScript);
-            }
+                CobSetup(cob, cornScript);
+            } else Destroy(cob);
 
-            int position = cobs.IndexOf(cornScript);
-            cornScript.CurrentSide = CornScript.Side.Top;
-            cornScript.audioController.Play();
-            cornScript.isPlaced = true;
-            Transform cobTransform = cob.transform;
-            cobTransform.position = cobPos[position];
-            cobTransform.rotation = Quaternion.Euler(0f,0f,cobRot[position]);
-            cob.GetComponent<PolygonCollider2D>().enabled = true;
-            cobTransform.parent = this.transform;
         } else Destroy(cob);
     }
-
     bool anyNull()
     {
-        bool isNull = true;
+        bool isNull = false;
         for(int x = 0; x < cobs.Count; x++)
         {
             if (cobs[x] == null)
@@ -130,6 +103,16 @@ public class PanScript : MonoBehaviour
         }
         return isNull;
     }
-
-
+    void CobSetup(GameObject cob, CornScript cornScript)
+    {
+        int position = cobs.IndexOf(cornScript);
+        cornScript.CurrentSide = CornScript.Side.Top;
+        cornScript.audioController.Play();
+        cornScript.isPlaced = true;
+        Transform cobTransform = cob.transform;
+        cobTransform.position = cobPos[position];
+        cobTransform.rotation = Quaternion.Euler(0f,0f,cobRot[position]);
+        cob.GetComponent<PolygonCollider2D>().enabled = true;
+        cobTransform.parent = this.transform;
+    }
 }
