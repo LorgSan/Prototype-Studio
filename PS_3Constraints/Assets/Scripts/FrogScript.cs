4af2588c7dcca6dd8499e51ad560bf522cd2aed3
Lorg <hikariame100@gmail.com>
2022-09-20 10:24:26 -0400

    lab result

--- PS_3Constraints/Assets/Scripts/FrogScript.cs
+++ PS_3Constraints/Assets/Scripts/FrogScript.cs
@@ -8,25 +8,42 @@ public class FrogScript : MonoBehaviour
     PlayerControls controls;
     Vector2 rightEyeMove;
     Vector2 leftEyeMove;
+
+    [Header("Movement")]
     [SerializeField] float speed;
     [SerializeField] float accelerationRight;
     [SerializeField] float accelerationLeft;
     [SerializeField] float maxSpeed;
     bool isMovingRight;
     bool isMovingLeft;
-    Transform rightEye;
-    Transform leftEye;
+
+    [Header("Reference Objects")]
+    [SerializeField] Transform rightEyeTarget;
+    [SerializeField] Transform leftEyeTarget;
     Rigidbody2D rbRight;
     Rigidbody2D rbLeft;
+    [SerializeField] Transform rightEye;
+    [SerializeField] Transform leftEye;
+    CircleCollider2D rightEyeCollider;
+    CircleCollider2D leftEyeCollider;
+    Vector3 rightEyePoint;
+    Vector3 leftEyePoint;
+    Vector3 rightTargetPos;
+    Vector3 leftTargetPos;
+    Transform rightEyeSprite;
+    Transform leftEyeSprite;
 
     void Awake()
     {
         controls = new PlayerControls();
         ControlsSetup();
-        rightEye = transform.GetChild(0);
-        leftEye = transform.GetChild(1);
-        rbRight = rightEye.GetComponent<Rigidbody2D>();
-        rbLeft = leftEye.GetComponent<Rigidbody2D>();
+        rbRight = rightEyeTarget.GetComponent<Rigidbody2D>();
+        rbLeft = leftEyeTarget.GetComponent<Rigidbody2D>();
+        rightEyeCollider = rightEye.GetComponent<CircleCollider2D>();
+        leftEyeCollider = leftEye.GetComponent<CircleCollider2D>();
+        rightEyeSprite = rightEye.GetChild(0);
+        leftEyeSprite = leftEye.GetChild(0);
+
     }
 
     void ControlsSetup()
@@ -36,39 +53,49 @@ public class FrogScript : MonoBehaviour
         controls.Gameplay.RightEye.performed += ctx => isMovingRight = true;
         controls.Gameplay.RightEye.canceled += ctx => rightEyeMove = Vector2.zero;
         controls.Gameplay.RightEye.canceled += ctx => isMovingRight = false;
+        controls.Gameplay.RightEye.canceled += ctx => accelerationRight = 1;
         controls.Gameplay.LeftEye.performed += ctx => leftEyeMove = ctx.ReadValue<Vector2>();
         controls.Gameplay.LeftEye.performed += ctx => isMovingLeft = true;
         controls.Gameplay.LeftEye.canceled += ctx => leftEyeMove = Vector2.zero;
         controls.Gameplay.LeftEye.canceled += ctx => isMovingLeft = false;
+         controls.Gameplay.LeftEye.canceled += ctx => accelerationLeft = 1;
     }
 
     void Update()
     {
-        Vector2 mR = new Vector2(rightEyeMove.x, rightEyeMove.y) * accelerationRight * Time.deltaTime * speed;
+
+        EyePosCalc();
+        //Vector2 mR = new Vector2(Mathf.Clamp(rightEyeMove.x, -6, 7), Mathf.Clamp(rightEyeMove.y, -1f, 8)) * accelerationRight * Time.deltaTime * speed;
+        float clampedRightX = Mathf.Clamp(leftEyeMove.x, -6, 7);
+        float clampedRightY = Mathf.Clamp(leftEyeMove.y, -1f, 8);
+        Vector2 mR = new Vector2(clampedRightX, clampedRightY) * accelerationRight * Time.deltaTime * speed;
         if (accelerationRight < maxSpeed) 
         {
-            accelerationRight += 1;
+            accelerationRight += 0.1f;
         }
 
-        rightEye.Translate(mR, Space.World);
-        Vector2 mL = new Vector2(leftEyeMove.x, rightEyeMove.y) * accelerationLeft * Time.deltaTime * speed;
+        rightEyeTarget.Translate(mR, Space.World);
+        //Vector2 mL = new Vector2(Mathf.Clamp(leftEyeMove.x, -6, 7), Mathf.Clamp(rightEyeMove.y, -1f, 8)) * accelerationLeft * Time.deltaTime * speed;
+        float clampedLeftX = Mathf.Clamp(rightEyeMove.x, -6, 7);
+        float clampedLeftY = Mathf.Clamp(rightEyeMove.y, -1f, 8);
+        Vector2 mL = new Vector2(clampedLeftX, clampedLeftY) * accelerationLeft * Time.deltaTime * speed;
         if (accelerationLeft < maxSpeed)
         {
-            accelerationLeft += 1;
+            accelerationLeft += 0.1f;
         }
-        leftEye.Translate(mL, Space.World);
+        leftEyeTarget.Translate(mL, Space.World);
     }
 
     void FixedUpdate()
     {
-        if (isMovingRight == true)
-        {
-            rbRight.velocity = new Vector2(rightEyeMove.x, rightEyeMove.y);  
-        }
-        if (isMovingLeft == true)
-        {
-            rbLeft.velocity = new Vector2(leftEyeMove.x, leftEyeMove.y);
-        }
+        // if (isMovingRight == true)
+        // {
+        //     rbRight.velocity = new Vector2(rightEyeMove.x, rightEyeMove.y);  
+        // }
+        // if (isMovingLeft == true)
+        // {
+        //     rbLeft.velocity = new Vector2(leftEyeMove.x, leftEyeMove.y);
+        // }
     }
 
     void RestartScene()
@@ -85,5 +112,15 @@ public class FrogScript : MonoBehaviour
     void OnDisable()
     {
         controls.Gameplay.Disable();
+    }
+
+    void EyePosCalc()   
+    {
+        Vector3 rightTargetPos = rightEyeTarget.position;
+        Vector3 leftTargetPos = leftEyeTarget.position;
+        rightEyePoint = rightEyeCollider.ClosestPoint(rightTargetPos);
+        leftEyePoint = leftEyeCollider.ClosestPoint(leftTargetPos);
+        leftEyeSprite.position = leftEyePoint;
+        rightEyeSprite.position = rightEyePoint;
     }
 }
