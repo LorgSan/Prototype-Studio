73d3a6d9a9644e0fbddff85fe3aa8977acfcf5b6
MominAman <momin.324@gmail.com>
2022-09-21 17:50:00 -0400

    Tongue logic

--- PS_3Constraints/Assets/Scripts/TongueScript.cs
+++ PS_3Constraints/Assets/Scripts/TongueScript.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class TongueScript : MonoBehaviour
 {
-    public Transform target;
+    public Transform target, tongueTipPos, tongueChildObj;
 
+    public float speed = 50.0f;
+
+    private Vector3 initialScale;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialScale = tongueChildObj.transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 _target = new Vector3(target.position.x,0,0);
-        transform.LookAt(_target);
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x -transform.position.x ) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, speed * Time.deltaTime);
+
+        ScaleTongue();
     }
+
+
+    void ScaleTongue()
+    {
+        //Scale the tongueChildObj to the distance between the tongueTipPos and the target
+        float distance = Vector3.Distance(tongueTipPos.position, target.position);
+        
+        Debug.Log(distance);
+
+        //Keep scaling the tongueChildObj until the distance is less than 0.1
+        if (distance > 0.1f)
+        {
+            tongueChildObj.transform.localScale += new Vector3(0, 0.001f, 0);
+        }
+    }
+
 }
