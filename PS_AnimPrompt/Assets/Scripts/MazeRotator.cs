46a93545d96e23fe62542190f6aa5df057e4f5a7
Lorg <hikariame100@gmail.com>
2022-11-03 16:18:59 -0400

    walls rotating

--- PS_AnimPrompt/Assets/Scripts/MazeRotator.cs
+++ PS_AnimPrompt/Assets/Scripts/MazeRotator.cs
@@ -6,12 +6,27 @@ public class MazeRotator : MonoBehaviour
 {
     [SerializeField] private int difficulty;
     public List<Transform> innerWalls;
+    float currentRotY;
+    float startRotY;
+    float endRotY;
+    bool rotated;
 
     public void RotateWalls()
     {
-        for (int i = 0; i < innerWalls.Count; i++)
+        if (rotated)
         {
-            
+            for (int i = 0; i < innerWalls.Count; i++)
+            {
+                innerWalls[i].GetComponent<WallRotate>().Rotate(true);
+            }
+            rotated = false;
+        } else
+        {
+            for (int i = 0; i < innerWalls.Count; i++)
+            {
+                innerWalls[i].GetComponent<WallRotate>().Rotate(true);
+            }
+            rotated = false;
         }
     }
 }
---
+++ PS_AnimPrompt/Assets/Scripts/WallRotate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRotate : MonoBehaviour
+{
+    IEnumerator RotateMe(Vector3 byAngles, float inTime) 
+    {    
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for(var t = 0f; t <= 1; t += Time.deltaTime/inTime) 
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        transform.rotation = toAngle;
+    }
+    public void Rotate (bool goingBack) {
+    
+        if (goingBack)
+        {
+            StartCoroutine(RotateMe(Vector3.up * -90, 0.8f));
+        } else StartCoroutine(RotateMe(Vector3.up * 90, 0.8f));
+
+   }
+ }
